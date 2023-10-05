ee95dc6c33116fc2f71b4f7adf5d7f0a803e6795
minht <minht@MINHKHUAT>
2023-10-05 16:36:48 +0700

    Add project files.

---
+++ BaiTap2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Bai2_2
+{
+    class BaiTapVeNha
+    {
+        static void Main(string[] args)
+        {
+            int x, hour, min, sec;
+            Console.WriteLine("Nhap vao so giay muon quy doi:");
+            x = Convert.ToInt32(Console.ReadLine());
+            hour = x / 3600;
+            min = (x % 3600) / 60;
+            sec = x - (hour * 3600) - (min * 60);
+            Console.WriteLine("So giay sau khi quy doi: {0:00}:{1:00}:{2:00}", hour, min, sec);
+            Console.ReadKey();
+        }
+
+    }
+}
